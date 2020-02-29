34070ce12f878639f8c6d420030d6106b9258281
rodrigo katz <rodrigogkatz@gmail.com>
2020-02-28 22:09:56 -0300

    ejercicio 12 de estadistica y censo

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -5,12 +5,12 @@ e ingresada por ventanas emergentes solamente
 cuadros de textos. 
 12.   Los datos requeridos son los siguientes:
 A.	Edad, entre 18 y 90 años inclusive.
-B.	Sexo, “M” para masculino y “F” para femenino
+B.	Sexo, M para masculino y F para femenino
 C.	Estado civil, 1-para soltero, 2-para casados, 3-para divorciados 
 y 4-para viudos
 D.	Sueldo bruto, no menor a 8000.
 E.	Número de legajo, numérico de 4 cifras, sin ceros a la izquierda.
-F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para 
+F.	Nacionalidad, A para argentinos, E para extranjeros, N para 
 nacionalizados.
  */
 function ComenzarIngreso () {
@@ -40,7 +40,7 @@ function ComenzarIngreso () {
         }
 
         while(sexo.toUpperCase() != "si".toUpperCase() || sexo.toUpperCase() != "no".toUpperCase()){
-            //B.Sexo, “M” para masculino y “F” para femenino
+            // B.Sexo, M para masculino y F para femenino
             sexo = prompt("ERROR! ingrese el sexo: M o F");
         }
 
