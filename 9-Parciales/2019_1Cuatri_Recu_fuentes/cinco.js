8d687be0a7c8d6887c1509551eb84e083b2d7943
rodrigogkatz <rodrigogkatz@gmail.com>
2020-02-29 12:33:54 -0300

    ejecicio cuatro.html y cinco.html del recuperatorio del parcial 2019

--- 9-Parciales/2019_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,57 @@
+/**
+ * 
+ * 
+ * 
+Bienvenidos (SOLO SWITCH). 
+Una empresa de viajes le solicita ingresar que continente le gustaria visitar 
+y la cantidad de días , la oferta dice que por día se cobra $100 , 
+que se puede pagar de todas las maneras 
+
+Si es América tiene un 15% de descuento y si ademas paga por débito 
+se le agrega un 10% mas de descuento 
+
+Si es África u Oceania tiene un 30% de descuento y si además paga por 
+mercadoPago o efectivo se le agrega un 15% mas de descuento 
+
+Si es Europa tiene un 20% de descuento y si ademas paga por débito se 
+e agrega un 15% , por mercadoPago un 10% y cualquier otro medio 5% 
+
+cualquier otro continente tiene un recargo del 20% 
+en cualquier continenete , si paga con cheque, se recarga un 15% de impuesto al cheque * 
+ * 
+ * 
+ */
+
 function mostrar()
 {
+    var importePorDia = 100;
+    var importeSinDescuento = 0;
+    var importeConDescuento = 0;
+    var importedescuentoPorFormaDePago = 0;
 
+    var queContinenteQuiereVisitar = prompt("ingresar que continente le gustaria visitar");
+    var cantidadDeDias = prompt("cantidad de días");
+    var formaDePago = prompt("forma de pago");
+
+
+    //100%  - 100
+    //15%   - ?
+    switch(queContinenteQuiereVisitar){
+        case 'America': 
+            importeSinDescuento = cantidadDeDias*importePorDia;
+            importeConDescuento = importeSinDescuento - (importeSinDescuento*0.15);
+            if(formaDePago=="debito"){
+                importedescuentoPorFormaDePago = importeConDescuento - (importeConDescuento*0.10);
+            }
+            break;
+        case 'Africa': 
+            //code
+            break;
+        case 'Europa': 
+            //code
+            break;
+        default: 
+            //code
+            break;
+    }
 }
--- 9-Parciales/2019_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2019_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,30 @@
+/**
+ * Bienvenidos (IF). 
+Pedir dos números y mostrar el resultado: 
+Si son iguales los muestro el cuadrado del numero. 
+Si el primero divisible por el segundo, los resto, 
+de lo contrario muestro solo el resto. 
+si el resto es mayor a 3(tres ) informarlo por alert.
+ */
+
 function mostrar()
 {
+    var numeroUno = parseInt(prompt("ingrese un numero"));
+    var numeroDos = parseInt(prompt("ingrese otro numero"));
 
+    if(numeroUno == numeroDos){
+        alert("el cuadrado del primer numero es "+(numeroUno*numeroUno)+ " y el cuadrado del segundo es"+(numeroDos*numeroDos));
+    }
+
+    if((numeroUno % numeroDos)==0){
+        alert("La resta de los numeros es igual a "+(numeroUno-numeroDos));
+    }else{
+        alert("Muestro solo el resto "+(numeroUno%numeroDos));
+    }
+
+    if(numeroUno%numeroDos>3){
+        alert("Si el resto es mayor a 3 lo muestro por alert. RESTO: "+(numeroUno%numeroDos));
+    }
+
+    
 }
